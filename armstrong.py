5ebf5c480c8e19e4db5f53debdcb4048b4bafd95
Sujitha03 <45787498+Sujitha03@users.noreply.github.com>
2019-05-30 12:13:12 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+n=int(input())
+m=n
+if(n<=100000):
+ remainder=0
+ while(n!=0):
+  num=n%10
+  remainder=remainder+num*num*num
+  n=n//10
+ if(m==remainder):
+  print("Yes")
+ else:
+  print("No")
