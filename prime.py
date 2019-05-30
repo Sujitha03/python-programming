1910817685e78803453baf1dfdaab646b10da71f
Sujitha03 <45787498+Sujitha03@users.noreply.github.com>
2019-05-30 11:25:26 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,14 @@
 n=int(input())
-if(n>1):
- for i in range(2,n):
-  if(n%i==0):
-   print("No")
-   break
+if(n<=1000):
+ if(n>1):
+  for i in range(2,n):
+   if(n%i==0):
+    print("No")
+    break
+  else:
+   print("Yes")
  else:
-  print("Yes")
+  print("No")
 else:
- print("No")
+ print("Out of range")
+  
